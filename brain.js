81fc403ed2f17eefa88861cc179a72f66a1afd18
Emmanuel uma <umaemmanuel62@gmail.com>
2022-11-08 11:05:59 +0100

    fixed the textarea bug

--- brain.js
+++ brain.js
@@ -1,33 +1,35 @@
-const iconsHouse = document.getElementById('iconsHouse');
-const minMenu = document.getElementById('minMenu');
-const menuBar = document.getElementById('menu');
-const links = document.getElementsByClassName('link');
-const searchIcon = document.getElementById('search');
-const closeIcon = document.getElementById('close');
-const form = document.getElementById('form');
+let nav = (function () {
+    const iconsHouse = document.getElementById('iconsHouse');
+    const minMenu = document.getElementById('minMenu');
+    const menuBar = document.getElementById('menu');
+    const links = document.getElementsByClassName('link');
+    const searchIcon = document.getElementById('search');
+    const closeIcon = document.getElementById('close');
+    const form = document.getElementById('form');
 
 
-menuBar.addEventListener('click', e => {
-    if (minMenu.style.height == '50vh') {
-        minMenu.style.height = '0vh';
-        for (let i = 0; i < links.length; i++) {
-            links[i].style.display = "none";
+    menuBar.addEventListener('click', e => {
+        if (minMenu.style.height == '50vh') {
+            minMenu.style.height = '0vh';
+            for (let i = 0; i < links.length; i++) {
+                links[i].style.display = "none";
+            }
+        }else {
+            minMenu.style.height = '50vh';
+            for (let i = 5; i => links.length; i--) {
+                if(i == -1) {break;}
+                links[i].style.display = "inline";
+            }
         }
-    }else {
-        minMenu.style.height = '50vh';
-        for (let i = 5; i => links.length; i--) {
-            if(i == -1) {break;}
-            links[i].style.display = "inline";
-        }
-    }
-})
-
-closeIcon.style.display = 'none';
-searchIcon.addEventListener('click', e => {
-    form.style.height = '100vh';
-    closeIcon.style.display = 'block';
-    closeIcon.addEventListener('click', e => {
-        form.style.height = '0vh';
-        (form.style.height == '0vh') ? closeIcon.style.display = 'none' :  closeIcon.style.display = 'block';
     })
-})
+
+    closeIcon.style.display = 'none';
+    searchIcon.addEventListener('click', e => {
+        form.style.height = '100vh';
+        closeIcon.style.display = 'block';
+        closeIcon.addEventListener('click', e => {
+            form.style.height = '0vh';
+            (form.style.height == '0vh') ? closeIcon.style.display = 'none' :  closeIcon.style.display = 'block';
+        })
+    })
+}) ()
